891d121d42cd46c7b5ac24e23d2742c45b1955bc
Charles Lao <charles.h.lao@gmail.com>
2023-04-14 08:55:33 +0800

    Add color template and font

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,7 +1,7 @@
 /* eslint-disable arrow-body-style */
 
-const todoItem = (title, description, dueDate, priority) => {
-    return { title, description, dueDate, priority };
+const todoItem = (title, description, dueDate, priority, project) => {
+    return { title, description, dueDate, priority, project };
 };
 
 export default todoItem;
