0b41492c349795328a21e9f97e6153701cf8aa6e
Charles Lao <charles.h.lao@gmail.com>
2023-04-17 15:48:12 +0800

    Create local storage for tasks and projects

--- src/inbox.js
+++ src/inbox.js
@@ -1,14 +1,32 @@
 const inbox = (projects, tasks) => {
   const inboxDiv = document.createElement("div");
+  inboxDiv.setAttribute("id", "right-container");
 
   const inboxHeading = document.createElement("h3");
   inboxHeading.textContent = "Inbox";
 
-  const inboxTask = document.createElement("p");
-  inboxTask.textContent = "Test";
-
   inboxDiv.appendChild(inboxHeading);
-  inboxDiv.appendChild(inboxTask);
+
+  if (tasks != null) {
+    for (let i = 0; i < tasks.length; i++) {
+      const taskDiv = document.createElement("div");
+      taskDiv.setAttribute("class", "task-div");
+
+      const btn = document.createElement("button");
+
+      const taskDetails = document.createElement("p");
+      taskDetails.textContent = tasks[i].title;
+
+      const taskDueDate = document.createElement("p");
+      taskDueDate.textContent = tasks[i].dueDate;
+
+      taskDiv.appendChild(btn);
+      taskDiv.appendChild(taskDetails);
+      taskDiv.appendChild(taskDueDate);
+
+      inboxDiv.appendChild(taskDiv);
+    }
+  }
 
   return inboxDiv.outerHTML;
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ import Task from "./todoItem";
 import "./styles/style.css";
 import inbox from "./inbox";
 
-// const testItem = Task('test title', 'test description', 'test duedate', 'test priority');
-
 const storageController = (() => {
-  const projectStorage = [];
-  const taskStorage = [];
+  // copy contents of local storage to the variables if they exist
+  let projectStorage = [];
+  let taskStorage = [];
+
+  if (localStorage.getItem("projectStorage") !== null) {
+    projectStorage = JSON.parse(localStorage.getItem("projectStorage"));
+  }
+
+  if (localStorage.getItem("taskStorage") !== null) {
+    taskStorage = JSON.parse(localStorage.getItem("taskStorage"));
+  }
+
+  const testItem = Task(
+    "test title",
+    "test description",
+    "test duedate",
+    "test priority",
+    "default"
+  );
+  // console.log(testItem);
+
+  taskStorage.push(testItem);
+  localStorage.setItem("taskStorage", JSON.stringify(taskStorage));
+
+  console.log(taskStorage);
 
   return {
     projectStorage,
